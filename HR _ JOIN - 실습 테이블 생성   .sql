645fc847c745b64d66088cae71acb6038ae26185
Jangwoo Park <p.jangwoo@gmail.com>
2023-07-04 16:24:17 +0900

    7월 4일 - 오후 4시

--- HR _ JOIN - 실습 테이블 생성   .sql
+++ HR _ JOIN - 실습 테이블 생성   .sql
@@ -1,6 +1,16 @@
+/* JOIN 실습 */ 
+drop table member10 cascade constraints; 
+drop table zipcode10 cascade constraints;
+drop table product10 cascade constraints;
+drop table orders10 cascade constraints;
 
-/* Simple Table 생성 */ 
-
+create table zipcode10 (
+    zipcode number(6) not null constraint PK_ZIPCODE_ZIPCODE Primary key, 
+    si_do varchar2(200) not null, 
+    gu_gun varchar2 (200) not null, 
+    bungi varchar2 (200) not null
+    ); 
+    
 create table member10 (
     m_id number(4) not null constraint PK_MEMBER10_M_ID Primary key, 
     pass varchar2(200) not null , 
@@ -9,12 +19,7 @@ create table member10 (
     zipcode number(6) null , 
     constraint FK_MEMBER10_ZIPCODE10 foreign key (zipcode) references zipcode10(zipcode)
     ); 
-create table zipcode10 (
-    zipcode number(6) not null constraint PK_ZIPCODE_ZIPCODE Primary key, 
-    si_do varchar2(200) not null, 
-    gu_gun varchar2 (200) not null, 
-    bungi varchar2 (200) not null
-    ); 
+
 create table product10 ( 
     pro_id number (6) not null constraint PK_PRODUCT10_PRO_ID primary key, 
     pro_name varchar2(200) not null, 
@@ -26,7 +31,21 @@ create table orders10 (
     ord_id number (6) not null constraint PK_ORDER10_ORD_ID Primary key, 
     m_id number(4) not null , 
     pro_id number(6) not null , 
+    orderdate date, 
         constraint FK_ORDER10_M_ID foreign key (m_id) references member10(m_id), 
         constraint FK_ORDER10_PRO_ID foreign key (pro_id) references product10(pro_id)
 ); 
-    
+
+
+-- 각 각의 테이블에 레코드 5개씩 저장후 JOIN 실습  : EQUIE JOIN, ANSI JOIN  
+--  구매 정보에 대해서 , 회원이름, 메일주소, 시도, 제품이름, 제품가격, 제품수량(cnt), 구매날짜를 출력 
+
+-- insert 한 쿼리, EQUIE JOIN, ANSI JOIN   <== 완료시간 6:20분까지 . p.jangwoo@gmail.com 
+
+
+
+
+
+
+
+
